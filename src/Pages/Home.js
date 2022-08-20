49bc75f4f48b4bd39ed06ddaa1a24e6fe650c179
Patrick <patrick@Patricks-MacBook-Pro.local>
2022-08-20 11:43:52 -0400

    created table and added columns

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,32 +1,45 @@
 import React, { useEffect, useState } from 'react'
 
+const TABLE_HEADERS = ["Coin", "Current Price", "Market Cap", "24 Hour Change (%)"];
+
 const Home = () => {
 
-    const [coinList, setCoinList] = useState(null);
+    const [coinList, setCoinList] = useState([]);
 
     useEffect(()=> {
         getCoins();
-        console.log(coinList);
     },[])
 
     const getCoins = async () => {
-        const data = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false").then(results=>results.json());
+        const data = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false").then(results=>results.json());
         setCoinList(data)
+        console.log(data)
     }
 
-    const renderCoins = () =>{
-        return (
-            <div>
-                {coinList.map(coin => (
-                    <div key={coin.id}>{coin.name}</div>
-                ))}
-            </div>
-        )
-    }
+    const renderTableHeader = () =>(
+        //console.log("RENDER HEADER")
+        TABLE_HEADERS.map(header => (
+            <th>{header}</th>
+        ))
+    )
+
+    const renderCoinsTable = () =>(
+        coinList.map(coin => (
+            <tr key={coin.id}>
+                <td><img src={coin.image} alt="Logo" width="20" height="20"/>{coin.name} ({coin.symbol}) </td>
+                <td>{coin.current_price}</td>
+                <td>{coin.market_cap}</td>
+                <td>{coin.price_change_percentage_24h}</td>
+            </tr>
+        ))
+    )
   return (
-    <div> 
-       {renderCoins()}
-    </div>
+    <table id="coins-table">
+        <tbody>
+            <tr>{renderTableHeader()}</tr>
+            {renderCoinsTable()} 
+        </tbody>
+    </table>
   )
 }
 
