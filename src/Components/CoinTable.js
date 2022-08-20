3814c364772e535b84f57206f91b885d774f4025
Patrick <patrick@Patricks-MacBook-Pro.local>
2022-08-20 12:05:33 -0400

    added styling for table

---
+++ src/Components/CoinTable.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import './CoinTable.style.css'
+
+const TABLE_HEADERS = ["Coin", "Current Price", "Market Cap", "24 Hour Change (%)"];
+
+const CoinTable = () => {
+
+    const [coinList, setCoinList] = useState([]);
+
+    useEffect(()=> {
+        getCoins();
+    },[])
+
+    const getCoins = async () => {
+        const data = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false").then(results=>results.json());
+        setCoinList(data)
+    }
+
+    const renderTableHeader = () =>(
+        TABLE_HEADERS.map(header => (
+            <th>{header}</th>
+        ))
+    )
+
+    const renderCoinsTable = () =>(
+        coinList.map(coin => (
+            <tr key={coin.id}>
+                <td><img src={coin.image} alt="Logo" width="20" height="20"/>{coin.name} ({coin.symbol}) </td>
+                <td>{coin.current_price}</td>
+                <td>{coin.market_cap}</td>
+                <td>{coin.price_change_percentage_24h}</td>
+            </tr>
+        ))
+    )
+  return (
+    <table id="coins-table">
+        <tbody>
+            <tr>{renderTableHeader()}</tr>
+            {renderCoinsTable()} 
+        </tbody>
+    </table>
+  )
+}
+
+export default CoinTable
